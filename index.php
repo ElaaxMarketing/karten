347b898525f2194688b34a51cb18551f90dde67b
Elaax-Marketing <d.bauer@elaax.net>
2021-07-15 15:28:24 +0200

    php einfürung

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="de" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="css/master.css">
+  </head>
+  <body>
+<div class="regionen">
+    <?php include 'karte.php'; ?>
+</div>
+  </body>
+</html>
