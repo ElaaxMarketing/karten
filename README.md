e766954b7d38c97f3bce8584ebdf948eb8580354
Elaax-Marketing <d.bauer@elaax.net>
2021-07-16 14:38:10 +0200

    Fix Halbe Sterne

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # karten
+Lizens:
+Dieses Projekt ist unter angebe von meine Namen + Firma und Webseitenlink zur freien Verwendung gestattet
+name:  Daniel Bauer von Elaax
+webseite:  www.elaax.de
+
+Anleitung:
+
+Wenn ihr mein Projekt verwenden wollt geht das wie folgt
+1. von github das Projekt herunterladen und am besten den kompletten Ordner ins Root Verzeichnis legen.
+2. nun habt ihr einen index.php dort ist ein include tag von php enthalten dies an die stelle kopierenan dem die Karten auftauchen sollen [<?php include './karte/karte.php'; ?>]
+3. Falz ihr den Ordner Karte in ein unterverzeichnis gelegt habt das includ anpassen (<?php include './fartzum ferzeichnis/karte/karte.php'; ?>)
+
+4. im Verzeichnis gibt es ein csv Datei diese ist zum erstellen von neuen Karten da.
+  Stern Bewertung ist wie folgt aufgebaut
+    a. sgut = 5 Sterne
+    b. sgut5 = 4.5 Sterne
+    c. bef, = 4 Sterne
+    d. bef5 = 3.5 Sterne
+    e. ausr = 3 Sterne
+    f. ausr5 = 2.5 Sterne
+    g. bef = 2 Sterne
+    h. bef5 = 1.5 Sterne
+    i. nausr = 1 Sterne
+    j. nausr5 = 0.5 Sterne
+    k. scheisse = 0. Sterne
+
+5. Bilder müssen im img Ortner liegen
+---
+bei anpassungswünschen gerne melden
--- karte.php
+++ karte.php
@@ -1,3 +1,8 @@
+<svg class="halbstern">
+  <linearGradient id="halp_stern" x1="0%" y1="0%" x2="100%" y2="0%">
+             <stop offset="50%" stop-color="hsl(61,100%,50%)"></stop>
+             <stop offset="50%" stop-color="hsl(0,0%,100%)"></stop>
+   </linearGradient>
 <?php
 $feld = file("kartendaten.csv");
 $zeil = 0;
@@ -22,7 +27,7 @@ $zeil = 0;
 
 
  ?>
-
+</svg>
 <div class="kart_<?php echo $karte;?>">
   <div class="kart_<?php echo $karte;?>__icon">
     <svg class="kart_<?php echo $karte;?>__icon--svg" viewBox="0 0 84 15" xmlns="http://www.w3.org/2000/svg">
@@ -40,10 +45,7 @@ $zeil = 0;
    </g>
    <g class="kart_<?php echo $karte;?>__icon--3">
      <path d="m42.041 0.03125-2.375 4.7812-5.0938 0.875 3.625 3.8438-0.78125 5.3438 4.5938-2.4062 4.5938 2.4375-0.75-5.3438 3.625-3.8438-5.0938-0.875-2.3438-4.8125zm0 0.09375"/>
-     <linearGradient id="halp_stern" x1="0%" y1="0%" x2="100%" y2="0%">
-       <stop offset="50%" stop-color="hsl(61,100%,50%)" />
-       <stop offset="50%" stop-color="hsl(0,0%,100%)" />
-    </linearGradient>
+   </g>
     </svg>
   </div>
   <img class="kart_<?php echo $karte;?>__img" src="./img/<?php echo $bild;?>" alt="<?php echo $alt;?>">
@@ -53,7 +55,7 @@ $zeil = 0;
     <a class="karte_<?php echo $karte;?>__content__ort--a" href="<?php echo $ortlink;?>">
       <svg class="karte_<?php echo $karte;?>__content__ort--svg" viewBox="0 0 592.99905 800.00001">
        <g transform="translate(0 -252.36)">
-        <path d="m296.52 252.36c-163.75-0.01067-296.51 132.73-296.52 296.48-0.0107 163.77 296.52 503.52 296.52 503.52s296.49-339.77 296.48-503.52c-0.0113-163.74-132.74-296.47-296.48-296.48zm0 18.974c153.26 8e-3 277.5 124.25 277.51 277.51-0.01 153.26-277.51 472.76-277.51 472.76s-277.54-319.49-277.55-472.76c8e-3 -153.27 124.27-277.52 277.55-277.51zm0 141.01a136.51 136.51 0 0 0 -136.54 136.5 136.51 136.51 0 0 0 136.54 136.54 136.51 136.51 0 0 0 136.5 -136.54 136.51 136.51 0 0 0 -136.5 -136.5zm0 13.941a122.58 122.58 0 0 1 122.55 122.55 122.58 122.58 0 0 1 -122.55 122.59 122.58 122.58 0 0 1 -122.59 -122.59 122.58 122.58 0 0 1 122.59 -122.55z" color="#000000" color-rendering="auto" fill="#ffffff" image-rendering="auto" shape-rendering="auto" solid-color="#000000" style="isolation:auto;mix-blend-mode:normal"/>
+        <path style="stroke-width: 2;" d="m296.52 252.36c-163.75-0.01067-296.51 132.73-296.52 296.48-0.0107 163.77 296.52 503.52 296.52 503.52s296.49-339.77 296.48-503.52c-0.0113-163.74-132.74-296.47-296.48-296.48zm0 18.974c153.26 8e-3 277.5 124.25 277.51 277.51-0.01 153.26-277.51 472.76-277.51 472.76s-277.54-319.49-277.55-472.76c8e-3 -153.27 124.27-277.52 277.55-277.51zm0 141.01a136.51 136.51 0 0 0 -136.54 136.5 136.51 136.51 0 0 0 136.54 136.54 136.51 136.51 0 0 0 136.5 -136.54 136.51 136.51 0 0 0 -136.5 -136.5zm0 13.941a122.58 122.58 0 0 1 122.55 122.55 122.58 122.58 0 0 1 -122.55 122.59 122.58 122.58 0 0 1 -122.59 -122.59 122.58 122.58 0 0 1 122.59 -122.55z"/>
        </g>
       </svg>
       <span class="karte_<?php echo $karte;?>__content__ort--p"><?php echo $ort;?></span></a>
